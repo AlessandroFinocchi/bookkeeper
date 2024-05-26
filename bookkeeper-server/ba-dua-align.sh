79f59d5073022cb06dd9edd13724979dd8bf27a5
AlessandroFinocchi <alessandro.22082001@gmail.com>
2024-05-26 16:03:40 +0200

    Ba-dua
    Badua working: it generates a report for each class tested defined in ba-sua-align.sh.
    Run 'mvn clean install -P isw2-ba-dua
    ' inside bookkeeper-server module to generate ba-dua report.

--- bookkeeper-server/ba-dua-align.sh
+++ bookkeeper-server/ba-dua-align.sh
@@ -1,2 +1,20 @@
-xmllint --format target/badua.xml > target/badua_pretty.xml
-rm target/badua.xml
+# Execute the shell code when ba-dua profile is activated
+
+input_file="badua.xml"
+modified_file="badua_pretty.xml"
+cookie_class="org/apache/bookkeeper/bookie/Cookie"
+booking_state_manager_class="org/apache/bookkeeper/bookie/BookieStateManager"
+
+xmllint --format target/$input_file > target/$modified_file
+
+rm target/$input_file
+cp "target/$modified_file" "target/badua_Cookie.xml"
+cp "target/$modified_file" "target/badua_BookieStateManager.xml"
+
+xmlstarlet ed -L -d \
+ "//class[not(contains(@name, '$cookie_class'))]" \
+ "target/badua_Cookie.xml"
+
+xmlstarlet ed -L -d \
+ "//class[not(contains(@name, '$booking_state_manager_class'))]" \
+ "target/badua_BookieStateManager.xml"
