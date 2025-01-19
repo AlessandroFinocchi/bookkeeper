9398425a5e6b6336227a2b1fe2a7222eb35e90a0
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-19 10:51:29 +0100

    upgrade WriteCachePutTest with pitest analysis

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBufAllocator;
 import org.apache.bookkeeper.util.collections.ConcurrentLongLongPairHashMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +18,8 @@ import static org.apache.bookkeeper.bookie.custom_utils.Utils.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class WriteCachePutTest {
     private enum WcType {
-        WRITTEN,
+        ONE_SEGMENT_WRITTEN,
+        HALF_SEGMENT_WRITTEN,
         NON_WRITTEN
     }
 
@@ -25,39 +27,48 @@ public class WriteCachePutTest {
         WriteCacheState t4invalid = new WriteCacheState(unpooledByteBufAllocator(),  0, 1, WcType.NON_WRITTEN);
         WriteCacheState validNonWritten1 = new WriteCacheState(unpooledByteBufAllocator(), 512, 256, WcType.NON_WRITTEN);
         WriteCacheState validNonWritten2 = new WriteCacheState(unpooledByteBufAllocator(), 16, 8, WcType.NON_WRITTEN);
-        WriteCacheState validWritten = new WriteCacheState(unpooledByteBufAllocator(), 512, 256, WcType.WRITTEN);
+        WriteCacheState validNonWritten3 = new WriteCacheState(unpooledByteBufAllocator(), 512, 1024, WcType.NON_WRITTEN);
+
+        WriteCacheState validOneSegWritten = new WriteCacheState(unpooledByteBufAllocator(), 256, 128, WcType.ONE_SEGMENT_WRITTEN);
+        WriteCacheState validHalfSegWritten = new WriteCacheState(unpooledByteBufAllocator(), 256, 128, WcType.HALF_SEGMENT_WRITTEN);
 
         return Stream.of(
-                // Constructor failed tests
-                Arguments.of(t4invalid, 1, 1, fullByteBuf(), false, null),
-
-                // Varying ledgerId
-                Arguments.of(validNonWritten1, -1, 1, fullByteBuf(), false, Exception.class),
-                Arguments.of(validNonWritten1,  0, 1, fullByteBuf(), true, null),
-
-                // Varying entryId
-                Arguments.of(validNonWritten1, 1, -1, fullByteBuf(), false, Exception.class),
-                Arguments.of(validNonWritten1, 1,  0, fullByteBuf(), true, null),
-
-                // Varying entry
-                Arguments.of(validNonWritten1, 1, 1, invalidReadIndexByteBuf(),   false, Exception.class),
-                Arguments.of(validNonWritten1, 1, 1, deallocatedByteBuf(),        false, Exception.class),
-                Arguments.of(validNonWritten1, 1, 1, null,                        false, Exception.class),
-
-                // Varying entry for different segment sizes
-                Arguments.of(validNonWritten1, 1, 1, fullByteBuf(),               true, null),
-                Arguments.of(validNonWritten2, 1, 1, fullByteBuf(),               false, null),
-                Arguments.of(validNonWritten1, 1, 1, emptyByteBuf(),              true, null),
-
-                // After jacoco
-                Arguments.of(validWritten, 1, 1, fullByteBuf(),              true, null),
-                Arguments.of(validWritten, 1, 3, fullByteBuf(),              true, null)
+//                // Constructor failed tests
+//                Arguments.of(t4invalid, 1, 1, fullByteBuf(), false, null),                                  // P1 passed
+//
+//                // Varying ledgerId
+//                Arguments.of(validNonWritten1, -1, 1, fullByteBuf(), false, Exception.class),               // P2 passed
+//                Arguments.of(validNonWritten1,  0, 1, fullByteBuf(), true, null),                           // P3 passed
+//
+//                // Varying entryId
+//                Arguments.of(validNonWritten1, 1, -1, fullByteBuf(), false, Exception.class),               // P4 passed
+//                Arguments.of(validNonWritten1, 1,  0, fullByteBuf(), true, null),                           // P5 passed
+//
+//                // Varying entry
+//                Arguments.of(validNonWritten1, 1, 1, invalidReadIndexByteBuf(),   false, Exception.class),  // P6 passed
+//                Arguments.of(validNonWritten1, 1, 1, deallocatedByteBuf(),        false, Exception.class),  // P7 passed
+//                Arguments.of(validNonWritten1, 1, 1, null,                        false, Exception.class),  // P8 passed
+//
+//                // Varying entry for different segment sizes
+//                Arguments.of(validNonWritten1, 1, 1, fullByteBuf(),               true, null),              // P9  passed
+//                Arguments.of(validNonWritten2, 1, 1, fullByteBuf(),               false, null),             // P10 passed
+//                Arguments.of(validNonWritten1, 1, 1, emptyByteBuf(),              true, null),              // P11 passed
+//
+//                // After jacoco
+//                Arguments.of(validOneSegWritten, 1, 1, fullByteBuf(),              true, null),   // P-J1 passed
+//                Arguments.of(validOneSegWritten, 1, 3, fullByteBuf(),              true, null),   // P-J2 passed
+//
+//                // After pitest
+//                Arguments.of(validNonWritten3,   1, 1, lenFullByteBuf(1024),     false, null),   // P-P1 passed
+//                Arguments.of(validOneSegWritten, 1, 1, lenFullByteBuf(128),       true, null),   // P-P1 passed
+                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),      true, null),   // P-P2 not passed
+                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),       false, null)    // P-P3 passed
         );
     }
 
     @ParameterizedTest
     @MethodSource("data")
-//    @Timeout(value = 5, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    @Timeout(value = 5, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
     public void put(WriteCacheState s, long ledgerId, long entryId, ByteBuf entry, boolean expectedReturn,
                           Class<Exception> expectedException) {
         WriteCache wc = new WriteCache(s.allocator, s.maxCacheSize, s.maxSegmentSize);
@@ -65,10 +76,10 @@ public class WriteCachePutTest {
 
         int firstPutEntryId = -1;
         int firstPutEntrySize = 0;
-        if(s.type == WcType.WRITTEN){
-            ByteBuf firstPutEntry = fullByteBuf();
+        if(s.type == WcType.ONE_SEGMENT_WRITTEN || s.type == WcType.HALF_SEGMENT_WRITTEN){
             firstPutEntryId = 2;
-            firstPutEntrySize = firstPutEntry.toString(StandardCharsets.UTF_8).length();
+            firstPutEntrySize = s.type == WcType.ONE_SEGMENT_WRITTEN ? wc.getMaxSegmentSize() : wc.getMaxSegmentSize()/2;
+            ByteBuf firstPutEntry = lenFullByteBuf(firstPutEntrySize);
             if(!wc.put(ledgerId, firstPutEntryId, firstPutEntry)) throw new RuntimeException("Put failed");
         }
 
@@ -83,7 +94,7 @@ public class WriteCachePutTest {
                 String expectedWrittenString = entry.toString(StandardCharsets.UTF_8);
                 long expectedSize  = expectedReturn ? expectedWrittenString.length() : 0;
                 long expectedCount = expectedReturn ? 1 : 0;
-                if (s.type == WcType.WRITTEN) {
+                if (s.type == WcType.ONE_SEGMENT_WRITTEN || s.type == WcType.HALF_SEGMENT_WRITTEN) {
                     expectedSize += firstPutEntrySize;
                     expectedCount += 1;
                 }
