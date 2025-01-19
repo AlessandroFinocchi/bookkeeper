bf252011a34a70ded43d0a516f35d7b8d77f4fcd
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-19 11:18:36 +0100

    some refactoring

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
@@ -25,44 +25,43 @@ public class WriteCachePutTest {
 
     private static Stream<Arguments> data() {
         WriteCacheState t4invalid = new WriteCacheState(unpooledByteBufAllocator(),  0, 1, WcType.NON_WRITTEN);
-        WriteCacheState validNonWritten1 = new WriteCacheState(unpooledByteBufAllocator(), 512, 256, WcType.NON_WRITTEN);
-        WriteCacheState validNonWritten2 = new WriteCacheState(unpooledByteBufAllocator(), 16, 8, WcType.NON_WRITTEN);
-        WriteCacheState validNonWritten3 = new WriteCacheState(unpooledByteBufAllocator(), 512, 1024, WcType.NON_WRITTEN);
+        WriteCacheState validTwoSegmentUnWritten = new WriteCacheState(unpooledByteBufAllocator(), 512, 256, WcType.NON_WRITTEN);
+        WriteCacheState validOneSegmentUnwritten = new WriteCacheState(unpooledByteBufAllocator(), 512, 1024, WcType.NON_WRITTEN);
 
         WriteCacheState validOneSegWritten = new WriteCacheState(unpooledByteBufAllocator(), 256, 128, WcType.ONE_SEGMENT_WRITTEN);
         WriteCacheState validHalfSegWritten = new WriteCacheState(unpooledByteBufAllocator(), 256, 128, WcType.HALF_SEGMENT_WRITTEN);
 
         return Stream.of(
                 // Constructor failed tests
-                Arguments.of(t4invalid, 1, 1, fullByteBuf(), false, null),                                  // P1 passed
+                Arguments.of(t4invalid, 1, 1, fullByteBuf(), false, null),                                          // P1 passed
 
                 // Varying ledgerId
-                Arguments.of(validNonWritten1, -1, 1, fullByteBuf(), false, Exception.class),               // P2 passed
-                Arguments.of(validNonWritten1,  0, 1, fullByteBuf(), true, null),                           // P3 passed
+                Arguments.of(validTwoSegmentUnWritten, -1, 1, fullByteBuf(), false, Exception.class),               // P2 passed
+                Arguments.of(validTwoSegmentUnWritten,  0, 1, fullByteBuf(), true, null),                           // P3 passed
 
                 // Varying entryId
-                Arguments.of(validNonWritten1, 1, -1, fullByteBuf(), false, Exception.class),               // P4 passed
-                Arguments.of(validNonWritten1, 1,  0, fullByteBuf(), true, null),                           // P5 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, -1, fullByteBuf(), false, Exception.class),               // P4 passed
+                Arguments.of(validTwoSegmentUnWritten, 1,  0, fullByteBuf(), true, null),                           // P5 passed
 
                 // Varying entry
-                Arguments.of(validNonWritten1, 1, 1, invalidReadIndexByteBuf(),   false, Exception.class),  // P6 passed
-                Arguments.of(validNonWritten1, 1, 1, deallocatedByteBuf(),        false, Exception.class),  // P7 passed
-                Arguments.of(validNonWritten1, 1, 1, null,                        false, Exception.class),  // P8 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, invalidReadIndexByteBuf(),   false, Exception.class),  // P6 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, deallocatedByteBuf(),        false, Exception.class),  // P7 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, null,                        false, Exception.class),  // P8 passed
 
                 // Varying entry for different segment sizes
-                Arguments.of(validNonWritten1, 1, 1, fullByteBuf(),               true, null),              // P9  passed
-                Arguments.of(validNonWritten2, 1, 1, fullByteBuf(),               false, null),             // P10 passed
-                Arguments.of(validNonWritten1, 1, 1, emptyByteBuf(),              true, null),              // P11 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, fullByteBuf(),               true, null),              // P9  passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, lenFullByteBuf(300),         false, null),             // P10 passed
+                Arguments.of(validTwoSegmentUnWritten, 1, 1, emptyByteBuf(),              true, null),              // P11 passed
 
                 // After jacoco
                 Arguments.of(validOneSegWritten, 1, 1, fullByteBuf(),              true, null),   // P-J1 passed
                 Arguments.of(validOneSegWritten, 1, 3, fullByteBuf(),              true, null),   // P-J2 passed
 
                 // After pitest
-                Arguments.of(validNonWritten3,   1, 1, lenFullByteBuf(1024),     false, null),   // P-P1 passed
-                Arguments.of(validOneSegWritten, 1, 1, lenFullByteBuf(128),       true, null),   // P-P1 passed
-//                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),    true, null),   // P-P2 not passed
-                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),     false, null)    // P-P3 passed
+                Arguments.of(validOneSegmentUnwritten,   1, 1, lenFullByteBuf(1024),     false, null),  // P-P1 passed
+                Arguments.of(validOneSegWritten, 1, 1, lenFullByteBuf(128),       true, null),          // P-P1 passed
+//                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),    true, null),          // P-P2 not passed
+                Arguments.of(validHalfSegWritten, 1, 1, lenFullByteBuf(128),     false, null)           // P-P3 passed
         );
     }
 
